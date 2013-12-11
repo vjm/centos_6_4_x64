ebdeb467250ccb8b9d48133f56f0de3e6b79527d
Vince Montalbano <vince.montalbano@gmail.com>
2013-12-11 08:09:52 +0200

    Initial Commit
    Signed-off-by: Vince Montalbano <vince.montalbano@gmail.com>

---
+++ scripts/centos/certs.sh
@@ -0,0 +1,2 @@
+# curl http://curl.haxx.se/ca/cacert.pem -o /etc/pki/tls/certs/ca-bundle.crt
+# http://serverfault.com/questions/472483/linux-centos-6-3-14pycurl-error-77-yum-repository-error
---
+++ scripts/common/vagrant.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+mkdir /home/vagrant/.ssh
+wget --no-check-certificate \
+    'https://github.com/mitchellh/vagrant/raw/master/keys/vagrant.pub' \
+    -O /home/vagrant/.ssh/authorized_keys
+chown -R vagrant /home/vagrant/.ssh
+chmod -R go-rwsx /home/vagrant/.ssh
+
+groupadd -r admin
+usermod -a -G admin vagrant
